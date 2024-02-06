6a1960d91e2f6170be6c1fa66613a35eaea44328
Matt Kuda <mattkuda@gmail.com>
2024-02-05 21:14:08 -0600

    Remove checkboxes from shuffle privew tile in WorkoutsScreen component

--- mobile/src/components/WorkoutsScreen/index.tsx
+++ mobile/src/components/WorkoutsScreen/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
-    VStack, Heading, Button, Pressable, Text, Box, HStack, Icon, IconButton, Checkbox,
+    VStack, Heading, Button, Pressable, Text, Box, HStack, Icon, IconButton,
 } from 'native-base';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons';
@@ -15,26 +15,6 @@ import { HorizontalWorkoutCards } from '../HorizontalWorkoutCards';
 import { useQueryWorkouts } from '../../hooks/useQueryWorkouts';
 import { featuredWorkouts } from '../../util/featuredWorkouts';
 import { formatTabatasCount } from '../../util/util';
-
-type CheckboxItemProps = {
-    label: string;
-    value: boolean;
-    setValue: (value: boolean) => void;
-    disabled: boolean;
-};
-
-const CheckboxItem: React.FC<CheckboxItemProps> = ({
-    label, value, setValue, disabled,
-}) => (
-    <Checkbox
-        isChecked={value}
-        isDisabled={disabled}
-        value=""
-        onChange={(): void => setValue(!value)}
-    >
-        {label}
-    </Checkbox>
-);
 
 type WorkoutsScreenNavigationProp = StackNavigationProp<TabNavigatorParamList, 'WorkoutsScreen'>;
 
@@ -87,15 +67,6 @@ export const WorkoutsScreen = (): JSX.Element => {
         navigation.navigate('DiscoverWorkoutsScreen');
     };
 
-    const shouldDisableCheckbox = (checkboxValue: boolean): boolean => {
-        const checkedCount = [shuffledWorkout.includeSettings.includeUpper,
-            shuffledWorkout.includeSettings.includeLower, shuffledWorkout.includeSettings.includeAbs,
-            shuffledWorkout.includeSettings.includeCardio]
-            .filter((val) => val).length;
-
-        return checkedCount === 1 && checkboxValue;
-    };
-
     const refetchData = async (): Promise<void> => {
         refetchMySavedWorkouts();
         refetchNewWorkouts();
@@ -125,56 +96,6 @@ export const WorkoutsScreen = (): JSX.Element => {
                                 ...prev,
                                 numberOfTabatas: prev.numberOfTabatas + 1,
                             }))}
-                        />
-                    </HStack>
-                    <HStack alignItems="center" justifyContent="space-between" mt={4}>
-                        <CheckboxItem
-                            disabled={shouldDisableCheckbox(shuffledWorkout.includeSettings.includeUpper)}
-                            label="Upper"
-                            setValue={(newValue): void => setShuffledWorkout((prev) => ({
-                                ...prev,
-                                includeSettings: {
-                                    ...prev.includeSettings,
-                                    includeUpper: newValue,
-                                },
-                            }))}
-                            value={shuffledWorkout.includeSettings.includeUpper}
-                        />
-                        <CheckboxItem
-                            disabled={shouldDisableCheckbox(shuffledWorkout.includeSettings.includeLower)}
-                            label="Lower"
-                            setValue={(newValue): void => setShuffledWorkout((prev) => ({
-                                ...prev,
-                                includeSettings: {
-                                    ...prev.includeSettings,
-                                    includeLower: newValue,
-                                },
-                            }))}
-                            value={shuffledWorkout.includeSettings.includeLower}
-                        />
-                        <CheckboxItem
-                            disabled={shouldDisableCheckbox(shuffledWorkout.includeSettings.includeAbs)}
-                            label="Abs"
-                            setValue={(newValue): void => setShuffledWorkout((prev) => ({
-                                ...prev,
-                                includeSettings: {
-                                    ...prev.includeSettings,
-                                    includeAbs: newValue,
-                                },
-                            }))}
-                            value={shuffledWorkout.includeSettings.includeAbs}
-                        />
-                        <CheckboxItem
-                            disabled={shouldDisableCheckbox(shuffledWorkout.includeSettings.includeCardio)}
-                            label="Cardio"
-                            setValue={(newValue): void => setShuffledWorkout((prev) => ({
-                                ...prev,
-                                includeSettings: {
-                                    ...prev.includeSettings,
-                                    includeCardio: newValue,
-                                },
-                            }))}
-                            value={shuffledWorkout.includeSettings.includeCardio}
                         />
                     </HStack>
                     <Button mt={4} onPress={handlePressQuickShuffle}>Create</Button>
