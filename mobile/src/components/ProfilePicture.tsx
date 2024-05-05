9ed066531a1a4b18dbc9f5665b8034edc84c5a3b
Matt Kuda <mattkuda@gmail.com>
2024-05-05 16:44:31 -0400

    Refactor ProfilePicture component to dynamically calculate font size based on avatar size

--- mobile/src/components/ProfilePicture.tsx
+++ mobile/src/components/ProfilePicture.tsx
@@ -7,16 +7,34 @@ interface ProfilePictureProps extends IAvatarProps {
     user: UserFullInfoModel | UserInfo;
 }
 
-export const ProfilePicture: React.FC<ProfilePictureProps> = ({ user, ...avatarProps }) => {
-    // Get the initials from the user's name, else use the first letter of the username
-    const initials = user?.firstName || user?.lastName ? `${user?.firstName?.charAt(0) ?? ''}${user?.lastName?.charAt(0) ?? ''}` : user?.username?.charAt(0) ?? '';
+const sizes = {
+    xs: 14,
+    sm: 20,
+    md: 24,
+    lg: 32,
+    xl: 40,
+    '1xl': 48,
+    '2xl': 56,
+    '3xl': 64,
+    '4xl': 72,
+    '5xl': 80,
+    '6xl': 96,
+};
+
+export const ProfilePicture: React.FC<ProfilePictureProps> = ({ user, size = 'md', ...avatarProps }) => {
+    const fontSize = sizes[size as keyof typeof sizes] || 24;
+
+    const initials = user?.firstName || user?.lastName ? `${user?.firstName?.charAt(0).toUpperCase() ?? ''}${user?.lastName?.charAt(0).toUpperCase() ?? ''}` : user?.username?.charAt(0).toUpperCase() ?? '';
 
     return (
         <Avatar
+            alignItems="center"
+            justifyContent="center"
+            size={size}
             source={user?.profilePictureUrl ? { uri: user.profilePictureUrl } : undefined}
             {...avatarProps}
         >
-            <Text color="white" fontSize="5xl" fontWeight="bold">
+            <Text color="white" fontSize={fontSize} fontWeight="bold">
                 {initials}
             </Text>
         </Avatar>
