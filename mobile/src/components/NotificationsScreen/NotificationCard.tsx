37676dc037ad68c0b425502ca06c9ddfb11da91d
Matt Kuda <mattkuda@gmail.com>
2024-06-06 19:36:57 -0400

    refactor: Update NotificationCard component
    - Update background color logic to use color tokens
    - Add navigation functionality to navigate to profile or post screen based on notification type

--- mobile/src/components/NotificationsScreen/NotificationCard.tsx
+++ mobile/src/components/NotificationsScreen/NotificationCard.tsx
@@ -4,18 +4,31 @@ import {
 } from 'native-base';
 import { formatDistanceToNow } from 'date-fns';
 import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { NotificationModel } from '../../types/notifications';
 import { ProfilePicture } from '../ProfilePicture';
+import { HomeScreenNavigationProp } from '../../types/navigationTypes';
 
 type NotificationCardProps = {
     notification: NotificationModel;
 };
 
 export const NotificationCard: React.FC<NotificationCardProps> = ({ notification }) => {
-    const backgroundColor = notification.read ? 'gray8' : 'gray7';
+    const backgroundColor = notification.read ? 'gray.900' : 'gray.700';
+
+    const navigation = useNavigation<HomeScreenNavigationProp>();
+
+    const navigateToPost = (): void => {
+        if (notification.type === 'follow') {
+            // Navigate to Profile
+            navigation.navigate('Profile', { userId: notification.targetId });
+        } else {
+            navigation.navigate('PostScreen', { postId: notification.targetId.toString() });
+        }
+    };
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={navigateToPost}>
             <Box backgroundColor={backgroundColor} borderRadius="md" borderWidth="1" mt="2" p="4">
                 <HStack alignItems="center" space={3}>
                     <ProfilePicture size="48px" user={notification?.initiatorUserInfo} />
